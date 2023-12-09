225196fa91bc546a45dbae15df88c63ff5e2e56c
Fernando Rafael Carretto Sosa <fernando91.sosa@hotmail.com>
2023-12-09 13:04:14 -0300

    Code updated to not use EJS anymore

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 
-// Set the view engine to EJS
-app.set("view engine", "ejs");
-
 // Serve the public folder as static files
 app.use(express.static("public"));
 
-// Render the index template with default values for weather and error
+// Route for homepage (You can remove this if it's not needed for the Flutter app)
 app.get("/", (req, res) => {
-  res.render("index", { weather: null, error: null });
+  res.send("Weather API is up and running");
 });
 
 // Handle the /weather route
@@ -20,28 +17,20 @@ app.get("/weather", async (req, res) => {
   const apiKey = process.env.OPENWEATHER_API_KEY;
   const APIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
-  let weather;
-  let error = null;
-
   try {
     const response = await axios.get(APIUrl);
 
-    // Check if the response contains a 'cod' (status code) of 200
     if (response.data.cod === 200) {
-      weather = response.data;
+      res.status(200).json(response.data);
     } else {
-      // If the cod is not 200, set an error message
-      error = response.data.message || "Error, Please try again";
+      res.status(404).json({ error: response.data.message || "City not found" });
     }
   } catch (err) {
-    weather = null;
-    error = "Error, Please try again";
+    res.status(500).json({ error: "Internal Server Error" });
   }
-
-  res.render("index", { weather, error });
 });
 
-// Start the server and listen on port 3000 or the value of the PORT environment variable
+// Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
